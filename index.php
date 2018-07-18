bcfbdee4c8edd3a7b75666757316c98a89fbaa7f
natasha.dzhuri@gmail.com <39678203+ndzhu@users.noreply.github.com>
2018-07-18 15:51:19 +0200

    new uploads

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="de">
+  <head>
+    <title>finteflinte.org</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div class="header">
+      <a href="https://mur.at/">
+        <img src="https://mur.at/image/logo/murat_logo.svg">
+      </a>
+    </div>
+    <div>
+      <h1>Willkommen bei finteflinte.org!</h1>
+      <h2>
+        Diese Seite wurde am Don Jul 12 15:54:48 CEST 2018 automatisch bei der Einrichtung des Servers angelegt.
+      </h2>
+    </div>
+    <div>
+<?php echo date('r') ?>
+    </div>
+  </body>
+</html>
+<!--
+  vim: ts=2 sw=2 expandtab
+-->
